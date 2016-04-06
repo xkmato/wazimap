910b8467bc9d35efe10a308a74a430c8c00d5cb2
Kenneth <matovken@gmial.com>
2016-04-06 13:57:24 +0300

    adding household data

--- wazimap_ug/profiles.py
+++ wazimap_ug/profiles.py
@@ -33,15 +33,66 @@ def get_demographics_profile(geo_code, geo_level, session):
     return final_data
 
 
+def get_households_profile(geocode, geo_level, session):
+    try:
+        light_source, total_households = get_stat_data('household distribution by light source', geo_level, geocode,
+                                                       session, table_fields=['household distribution by light source'])
+        energy_source, _ = get_stat_data('household distribution by energy source', geo_level, geocode, session,
+                                         table_fields=['household distribution by energy source'])
+    except LocationNotFound:
+        household_dist, total_households = {}, 1
+        light_source = {}
+        energy_source = {}
+
+    total_electrified_lighting = 0
+    for data, value in light_source.get('Electricity', {}).iteritems():
+        if data == 'numerators':
+            total_electrified_lighting += value['this']
+
+    total_charcoal_energy = 0
+    for data, value in energy_source.get('Charcoal', {}).iteritems():
+        if data == 'numerators':
+            total_charcoal_energy += value['this']
+
+    final_data = {
+        'light_source_distribution': light_source,
+        'electrified_lighting': {
+            'name': 'Lighting with Electricity',
+            'numerators': {'this': total_electrified_lighting},
+            'values': {'this': round(total_electrified_lighting / total_households * 100, 2)}
+        },
+        'energy_source_distribution': energy_source,
+        'charcoal_energy': {
+            'name': 'Cooking with Charcoal',
+            'numerators': {'this': total_charcoal_energy},
+            'values': {'this': round(total_charcoal_energy / total_households * 100, 2)}
+        },
+        'total_households': {
+            "name": "Households",
+            "values": {"this": total_households}
+        }
+    }
+    return final_data
+
+
+PROFILE_SECTIONS = ['demographics', 'households']
+
+
 def get_profile(geo_code, geo_level, profile_name=None):
     session = get_session()
 
     try:
         geo_summary_levels = geo_data.get_summary_geo_info(geo_code, geo_level)
-        data = {'demographics': get_demographics_profile(geo_code, geo_level, session)}
+        data = {}
 
-        for level, code in geo_summary_levels:
-            merge_dicts(data['demographics'], get_demographics_profile(code, level, session), level)
+        for section in PROFILE_SECTIONS:
+            function_name = 'get_%s_profile' % section
+            if function_name in globals():
+                func = globals()[function_name]
+                data[section] = func(geo_code, geo_level, session)
+
+                for level, code in geo_summary_levels:
+                    merge_dicts(data[section], func(code, level, session), level)
         return data
 
     finally:
--- wazimap_ug/tables.py
+++ wazimap_ug/tables.py
@@ -3,3 +3,5 @@ from wazimap.data.tables import FieldTable
 FieldTable(['rural or urban'], table_per_level=False)
 FieldTable(['sex'], table_per_level=False)
 FieldTable(['household'], table_per_level=False)
+FieldTable(['household distribution by energy source'], table_per_level=False)
+FieldTable(['household distribution by light source'], table_per_level=False)
