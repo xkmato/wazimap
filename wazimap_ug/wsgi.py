0d5a9713fcbbd4fe91feae7b951b66cbb28013a9
Kenneth <matovken@gmial.com>
2016-04-26 14:50:06 +0300

    simple fix in wsgi

--- wazimap_ug/wsgi.py
+++ wazimap_ug/wsgi.py
@@ -1,5 +1,5 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wazimap.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wazimap_ug.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
